b26f8185e51f9468b5fe6875b58ab77a905758b2
Oleksandr Tkachenko <tkachenko@encrypto.cs.tu-darmstadt.de>
2020-05-26 15:04:29 +0200

    dummy find relic cmake script

---
+++ cmake/FindRelic.cmake
@@ -0,0 +1,7 @@
+find_package(PkgConfig)
+
+set(RLC_LIBRARY "${PSI_ANALYTICS_BINARY_ROOT}/extern/relic/lib/librelic_s.a")
+set(Relic_INCLUDE_DIR "${PSI_ANALYTICS_BINARY_ROOT}/include")
+set(RLC_INCLUDE_DIR ${Relic_INCLUDE_DIR})
+
+find_package_handle_standard_args(Relic "Found relic" Relic_INCLUDE_DIR)
